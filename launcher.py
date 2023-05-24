d8ec6861c245317a5387cb2d73b9075ca865bd0f
Albert Bargalló i Sales <110198818+AlbertBS2@users.noreply.github.com>
2023-05-24 19:46:37 +0200

    launcher.py

--- launcher.py
+++ launcher.py
@@ -1,33 +1,33 @@
 # Introduce the name of the title do you want your graph to has as 'graph_title'
-title = 'F1'
+title = ''
 # Introduce the name of your 3 results Excel files as 'name_of_file.xlsx'
-filename0 = 'HT400/HT400-15min-F1-1.xlsx'
-filename1 = 'HT400/HT400-15min-F1-2.xlsx'
-filename2 = 'HT400/HT400-15min-F1-3.xlsx'
+filename0 = 
+filename1 = 
+filename2 = 
 
-filename3 = 'HT450/HT450-15min-F1-1.xlsx'
-filename4 = 'HT450/HT450-15min-F1-2.xlsx'
-filename5 = 'HT450/HT450-15min-F1-3.xlsx'
+filename3 = 
+filename4 = 
+filename5 = 
 
-filename6 = 'NoHT/NoHT-F1-1.xlsx'
-filename7 = 'NoHT/NoHT-F1-2.xlsx'
-filename8 = 'NoHT/NoHT-F1-3.xlsx'
+filename6 = 
+filename7 = 
+filename8 = 
 
-filename9 = 'HT400/HT400-45min-F1-1.xlsx'
-filename10 = 'HT400/HT400-45min-F1-2.xlsx'
-filename11 = 'HT400/HT400-45min-F1-3.xlsx'
+filename9 = 
+filename10 = 
+filename11 = 
 
-filename12 = 'HT450/HT450-45min-F1-1.xlsx'
-filename13 = 'HT450/HT450-45min-F1-2.xlsx'
-filename14 = 'HT450/HT450-45min-F1-3.xlsx'
+filename12 = 
+filename13 = 
+filename14 = 
 
-filename15 = 'GoTaper/B1-1.xlsx'
-filename16 = 'GoTaper/B1-2.xlsx'
-filename17 = 'GoTaper/B1-3.xlsx'
+filename15 = 
+filename16 = 
+filename17 = 
 
-filename18 = 'ProTaper/PG-F1-1.xlsx'
-filename19 = 'ProTaper/PG-F1-2.xlsx'
-filename20 = 'ProTaper/PG-F1-3.xlsx'
+filename18 = 
+filename19 = 
+filename20 = 
 
 
 
@@ -45,3 +45,7 @@ project.graph(title,
               filename12, filename13, filename14,
               filename15, filename16, filename17,
               filename18, filename19, filename20)
+
+project.mean(filename0, filename1, filename2)
+
+project.std(filename0, filename1, filename2)
